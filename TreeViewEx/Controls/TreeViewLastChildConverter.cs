6e7c974b56e33f943acd4a67398078bb53175a12
evobali <balazs.sebo.ext@evosoft.com>
2016-10-20 08:50:03 +0200

    Lines added, alignment adjustment needed

---
+++ TreeViewEx/Controls/TreeViewLastChildConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace System.Windows.Controls
+{
+    class TreeViewLastChildConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            TreeViewExItem item = (TreeViewExItem)value;
+            ItemsControl ic = ItemsControl.ItemsControlFromItemContainer(item);
+            return ic.ItemContainerGenerator.IndexFromContainer(item) == ic.Items.Count - 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}
