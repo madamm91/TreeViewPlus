3f9a2be1d1c3d8b407645041e24ef7a1750d2fce
evobali <balazs.sebo.ext@evosoft.com>
2016-10-21 10:39:10 +0200

    Root lines hidden

---
+++ TreeViewEx/Controls/RootLinesVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace System.Windows.Controls
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public sealed class RootLinesVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            TreeViewExItem item = (TreeViewExItem)value;
+            return item.ParentTreeViewItem == null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}
